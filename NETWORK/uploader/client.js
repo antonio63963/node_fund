3d091fd50a0ebb7696ff6a063cb812ee05cd7e2e
a.fomin <a.fomin@anc.net.ua>
2026-04-12 15:11:03 +0300

    uploader backpressure + progress bar

--- NETWORK/uploader/client.js
+++ NETWORK/uploader/client.js
@@ -1,24 +1,47 @@
 const net = require("node:net");
 const fs = require("node:fs/promises");
+const path = require("node:path");
 
 const client = net.createConnection({ port: 5050, host: "::1" }, async () => {
-  const fd = await fs.open("clientFile.txt", "r");
+  //при запуске node client.js <fileName> указываем 3-й аргумент файл
+  console.log("PROC: ", process.argv);
+  const filePath = process.argv[2];
+  if (filePath == null) {
+    console.log("Запустите программу с 3-м аргументом - имя файла!");
+    process.exit(0);
+  }
+  const fileName = path.basename(filePath);
+  console.log("FILE NAME: ", fileName);
+  client.write(`fileName: ${fileName}|||`);
+
+  const fd = await fs.open(filePath, "r");
   const readStream = fd.createReadStream();
+
+  // for showing the upload progress
+  let uploaderPercentage = 0;
+  let bytesUploaded = 0;
+  const fileSize = (await fd.stat()).size;
 
   readStream.on("data", (chunk) => {
     console.log("STREAM: ", chunk.toString());
     if (!client.write(chunk)) {
-      client.pause();
+      readStream.pause();
+    }
+    bytesUploaded += chunk.length;
+    let stateUpload = Math.floor((bytesUploaded / fileSize) * 100);
+    if (stateUpload != uploaderPercentage) {
+      uploaderPercentage = stateUpload;
+      console.log("Uploading: ", uploaderPercentage, "%");
     }
   });
 
-  readStream.on("drain", () => {
+  client.on("drain", () => {
     readStream.resume();
   });
 
   readStream.on("end", () => {
     fd.close();
-    readStream.close();
+    readStream.destroy();
     client.end();
   });
 });
--- NETWORK/uploader/server.js
+++ NETWORK/uploader/server.js
@@ -5,35 +5,61 @@ const server = net.createServer(() => {});
 
 server.on("connection", (socket) => {
   console.log("NEW CONNECTION.");
+
   let fd;
   let fileStream;
+  let dataBuffer = Buffer.alloc(0);
+
   socket.on("data", async (data) => {
-    console.log("CHANK: ", data.toString());
     if (fd == null) {
+      dataBuffer = Buffer.concat([dataBuffer, data]);
+      const { fileName, buff } = _ifDataIsFileName(dataBuffer);
       //на открытие файла уйдет какое то время и могут прийти новые данные
-      socket.pause();
-      fd = await fs.open(`storage/test.txt`, "w");
-      fileStream = fd.createWriteStream();
-      fileStream.write(data);
-      socket.resume(); // возвращаю получение данных
-    }else {
+      if (fileName) {
+        socket.pause();
+        try {
+          fd = await fs.open(`storage/${fileName.toString().trim()}`, "w");
+          fileStream = fd.createWriteStream();
+          if (buff != null && buff.length) {
+            fileStream.write(buff);
+          }
+          fileStream.on("drain", () => {
+            socket.resume();
+          });
+        } catch (error) {
+          console.error("File Open Error:", err);
+          socket.destroy();
+        } finally {
+          socket.resume(); // возвращаюсь к получению данных
+        }
+      }
+    } else {
       //заполнился
       if (!fileStream.write(data)) {
-        fileStream.pause();
+        socket.pause();
       }
-      fileStream.on("drain", (data) => {
-        fileStream.resume();
-      });
-
     }
   });
 
-  socket.on("end", () => {
+  socket.on("end", async () => {
     console.log("end...");
-    fd.close();
-    fileStream.close();
+    if (fd) await fd.close();
+    if (fileStream) fileStream.end();
   });
 });
+
+function _ifDataIsFileName(data) {
+  const idxSeparator = data.indexOf("|||");
+  if (idxSeparator == -1) {
+    return { fileName: null, buff: data };
+  }
+
+  const nameHeader = data.subarray("fileName: ".length, idxSeparator);
+  console.log("NAME: ", nameHeader);
+  const restBuff = data.subarray(idxSeparator + "|||".length);
+
+  return { fileName: nameHeader, buff: restBuff };
+}
 
 server.listen(5050, "::1", () => {
   console.log("SERVER HAS RAN.", server.address());
