53a9d6bd4967eebe7627949921fa65a53c3b33eb
a.fomin <a.fomin@anc.net.ua>
2026-06-04 17:59:04 +0300

    video editor lint

--- unix/video-editor/lib/util.js
+++ unix/video-editor/lib/util.js
@@ -1,7 +1,7 @@
-const fs = require('node:fs/promises');
+const fs = require("node:fs/promises");
 
 const util = {
-  deleteFile : async function (path) {
+  deleteFile: async function (path) {
     try {
       await fs.unlink(path);
     } catch (error) {
@@ -9,15 +9,15 @@ const util = {
     }
   },
 
-  deleteFolder : async function (path) {
+  deleteFolder: async function (path) {
     try {
-      console.log('DEL PATH: ', path);
-      await fs.rm(path, {recursive: true});
+      console.log("DEL PATH: ", path);
+      await fs.rm(path, { recursive: true });
     } catch (error) {
-      console.log('ERROR: ', error);
+      console.log("ERROR: ", error);
       //do nothing
     }
-  }
-}
+  },
+};
 
 module.exports = util;
