913ae02b09cf939d557ee85a04ea5d6f447821de
a.fomin <a.fomin@anc.net.ua>
2026-06-06 15:15:24 +0300

    prime generator big int

--- THREADS/prime_generator/app.js
+++ THREADS/prime_generator/app.js
@@ -1,9 +1,34 @@
-const primeGenerator = require('./prime_generator');
-const {performance} = require('perf_hooks');
+const { Worker, workerData } = require("node:worker_threads");
 
-const start = performance.now();
+const THREADS = 4;
+const count = 200;
 
-console.log(primeGenerator(10, 100_000_000_000_000n));
+let completed = 0;
+const result = [];
 
-const end = performance.now() - start;
-console.log('TIME: ', end , ' ms');
+for (let i = 0; i < THREADS; i++) {
+  const thread1 = new Worker("./calc_thread.js", {
+    workerData: {
+      threadNum: i+1,
+      count: count / THREADS,
+      start: 100_000_000_000_000n + BigInt(i * 300),
+    },
+  });
+  console.log("THREAD WAS STARTED: ", thread1.threadId);
+  thread1.on("message", (msg) => {
+    result.push(msg.numbers);
+  });
+  thread1.on("error", (error) => {
+    console.error("Error: ", error);
+  });
+  thread1.on("exit", (code) => {
+    console.log("Worker ", thread1.threadId, " exit with code ", code);
+    completed++;
+    if(completed === THREADS) {
+      console.log(result.sort())
+    }
+    if(code !=0) {
+      console.error('ERRROR....')
+    }
+  });
+}
